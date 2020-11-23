73da71dede34f360de6dacf4d7ed00ec01cb8ccc
Bob <bobluppes@live.nl>
2020-11-23 23:34:18 +0100

    Naive implementation of JNI for filter eval

---
+++ NativeFilter.cpp
@@ -0,0 +1,8 @@
+#include <jni.h>       // JNI header provided by JDK
+#include <iostream>    // C++ standard IO header
+#include "com_dremio_sabot_op_filter_FilterTemplateAccelerated.h"  // Generated
+using namespace std;
+
+JNIEXPORT jboolean JNICALL Java_com_dremio_sabot_op_filter_FilterTemplateAccelerated_doNativeEval(JNIEnv *, jobject) {
+  return true;
+}
